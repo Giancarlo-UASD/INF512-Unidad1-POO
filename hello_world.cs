3b744dc16152fd949cae9cff30dd774097e1b589
Giancarlo-UASD <100667358@est.uasd.edu.do>
2026-09-01 16:12:21 -0400

    Ahora agregamos un segundo Hello World

--- hello_world.cs
+++ hello_world.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
     class Program{
         static void Main(string[] args){
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
         }
     }
 }
